2c22a8c068ae4dcb4fc6fc25d37906104f4a35f1
Leonard Lesinski <84378319+Le0X8@users.noreply.github.com>
2024-07-03 16:42:05 +0200

    Updated JS build

--- dist/index.js
+++ dist/index.js
@@ -244,14 +244,12 @@ ${result.mismatch.expected}\`\`\``,
                                         .then((_) => __awaiter(this, void 0, void 0, function* () {
                                         _;
                                         const title = `Format code using rustfmt for ${head.sha}`;
-                                        const body = `
-                          The code for commit \`${head.sha}\` on \`${head.ref.replace("refs/heads/", "")}\` has been formatted automatically using [rustfmt](https://github.com/rust-lang/rustfmt).
-                          Please review the changes and merge if everything looks good.
+                                        const body = `The code for commit \`${head.sha}\` on \`${head.ref.replace("refs/heads/", "")}\` has been formatted automatically using [rustfmt](https://github.com/rust-lang/rustfmt).
+Please review the changes and merge if everything looks good.
 
-                          ---
+---
 
-                          Delete the \`${ref.replace("refs/heads/", "")}\` branch after merging or closing the pull request.
-                        `;
+Delete the \`${ref.replace("refs/heads/", "")}\` branch after merging or closing the pull request.`;
                                         return octokit.rest.pulls.create(Object.assign(Object.assign({}, context.repo), { title, head: ref.replace("refs/heads/", ""), base: head.ref.replace("refs/heads/", ""), body }));
                                     }));
                                 }));
